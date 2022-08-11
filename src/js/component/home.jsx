7a0bc2b8299c1499d6030e3bc4166f72d87a195b
sneelyg <santiagoneely@gmail.com>
2022-08-11 02:03:19 +0000

    all done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import {Luces} from "./luces.jsx"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export const Home = () => {
